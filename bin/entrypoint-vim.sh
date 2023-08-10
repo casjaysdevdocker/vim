08f7d463dfdfd21ede1e8beb65431899313a2ed2
casjay <git-admin@casjaysdev.com>
2023-08-10 19:58:01 -0400

    🗃️ Updated domain names 🗃️
    bin/entrypoint-vim.sh
    Dockerfile
    LICENSE.md
    rootfs/usr/local/bin/entrypoint.sh
    rootfs/usr/local/bin/start-vim.sh

--- bin/entrypoint-vim.sh
+++ bin/entrypoint-vim.sh
@@ -2,7 +2,7 @@
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ##@Version       : 202202021753-git
 # @Author        : Jason Hempstead
-# @Contact       : jason@casjaysdev.com
+# @Contact       : jason@casjaysdev.pro
 # @License       : WTFPL
 # @ReadME        : entrypoint-vim.sh --help
 # @Copyright     : Copyright: (c) 2022 Jason Hempstead, Casjays Developments
